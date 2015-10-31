90add181310b384e4c1e91a9a1189f58d785e6a9
ensorrow <1121264060qq.com>
2015-10-31 16:46:46 +0800

    添加弹框js

---
+++ js/popup.js
@@ -0,0 +1,11 @@
+var basicAlert='<div data-role="popup" data-position-to="window" id="popup"><p>alert content</p></div>',
+	dialogAlert='<div data-role="popup" data-position-to="window" data-transition="pop" data-dismissible="false" id="popup" style="min-width:200px;"><div style="text-align:center;background:#32acea;padding:6px 0;"><h1 style="font-size:18px;color:#fff;">警告</h1></div><div style="padding:6px;"><p style="padding:6px 0;">确认付款？</p><a href="#" class="ui-btn ui-btn-inline" data-rel="back">取消</a><a href="#" class="ui-btn ui-btn-inline" data-rel="back">确认</a></div></div>';
+function alertMsg(msg,alertType){
+	if($('#popup')){$('#popup').remove();}//动态插入，开始前检测之前插入的并删除
+	var msg=msg||'错误！';//传入错误信息
+	var alertType=alertType||basicAlert;//传入弹框类型，默认为basicAlert,可选为dialogAlert
+	$(alertType).appendTo($('#main'));//插入文档
+	$('#popup p').html(msg);//修改错误信息
+	$('#popup').popup();//加载jqm popup组件
+	$('#popup').popup('open');//弹出框		
+}
